5968a6a3bfc0ab8b4ed1c4c514c6b0de6730bf4a
asifbasha61 <asif.abdul@tibilsolutions.com>
2023-01-23 12:03:08 +0530

    repo branch changed

--- shell_scripts/repository_clone.sh
+++ shell_scripts/repository_clone.sh
@@ -5,17 +5,17 @@ MY_PATH="microservices"
 cd "$MY_PATH"
 git clone https://github.com/Sunbird-cQube/spec-ms.git
 cd "spec-ms"
-git checkout dev
+git checkout stable-Jan23
 cd ../../
 
 cd "$MY_PATH"
 git clone https://github.com/Sunbird-cQube/ingestion-ms.git
 cd "ingestion-ms"
-git checkout dev
+git checkout stable-Jan23
 cd ../../
 
 cd "$MY_PATH"
 git clone https://github.com/Sunbird-cQube/generator-ms.git
 cd "generator-ms"
-git checkout dev
+git checkout stable-Jan23
 cd ../../
