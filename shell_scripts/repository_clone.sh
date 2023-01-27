edd49a6dc3b4655686c70c1db0eee3832da655a7
asifbasha61 <asif.abdul@tibilsolutions.com>
2023-01-27 14:51:06 +0530

    compose file changes

--- shell_scripts/repository_clone.sh
+++ shell_scripts/repository_clone.sh
@@ -1,40 +1,44 @@
 #!/bin/bash
 
 MY_PATH=microservices
+REPOSITORY=dev
 
 cd "$MY_PATH"
 if ! [[ -d spec-ms ]]; then
 	git clone https://github.com/Sunbird-cQube/spec-ms.git
 	cd "spec-ms"
-	git checkout dev
-	cd ../
+	git checkout $REPOSITORY
+	cd ../../
 else
 	cd "spec-ms"
-	git checkout dev
-        cd ../
+	git checkout $REPOSITORY
+	git pull
+        cd ../../
 fi
 
+cd "$MY_PATH"
 if ! [[ -d ingestion-ms ]]; then
 	git clone https://github.com/Sunbird-cQube/ingestion-ms.git
 	cd "ingestion-ms"
-	git checkout dev
-	cd ../
+	git checkout $REPOSITORY
+	cd ../../
 else
 	cd "ingestion-ms"
-	git checkout dev
-	cd ../
+	git checkout $REPOSITORY
+	git pull
+	cd ../../
 fi
 
+cd "$MY_PATH"
 if ! [[ -d generator-ms ]]; then
 	git clone https://github.com/Sunbird-cQube/generator-ms.git
 	cd "generator-ms"
-	git checkout dev
+	git checkout $REPOSITORY
 	cd ../../
 else 
-       cd "$MY_PATH"
 	cd "generator-ms"
-	git checkout dev
+	git checkout $REPOSITORY
+	git pull
 	cd ../../
 
 fi
-
