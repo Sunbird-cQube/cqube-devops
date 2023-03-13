1a1caeddcd6255e73acc97ec1518079946739c8d
ayesha-siddiq123 <ayesha.siddiq@tibilsolutions.com>
2023-03-13 12:43:33 +0530

    validation done for program_selector and generating the program_selector.yml file

---
+++ shell_scripts/program_selector.sh
@@ -0,0 +1,205 @@
+#!/bin/bash
+
+check_studentAttendance(){
+while true
+do
+echo -e "\e[0;36m${bold}Hint: Enter true or false to enable studentAttendance program ${normal}"
+echo -e "\e[0;38m${bold}please enter true or false.${normal}"
+read student_Attendance
+        if ! [[ $student_Attendance == "true" || $student_Attendance == "false" ]]; then
+        echo -e "\e[0;31m${bold}Error - Please enter either true or false ${normal}"; fail=1
+                        else
+                        printf "studentAttendance: $student_Attendance\n" >> program_selector.yml
+                        break;
+
+        fi
+done
+}
+
+check_teacherAttendance(){
+while true
+do
+echo -e "\e[0;36m${bold}Hint: Enter true or false to enable teacherAttendance program ${normal}"
+echo -e "\e[0;38m${bold}please enter true or false.${normal}"
+read teacher_Attendance
+        if ! [[ $teacher_Attendance == "true" || $teacher_Attendance == "false" ]]; then
+        echo -e "\e[0;31m${bold}Error - Please enter either true or false ${normal}"; fail=1
+                        else
+                        printf "teacherAttendance: $teacher_Attendance\n" >> program_selector.yml
+                        break;
+
+        fi
+done
+}
+
+check_reviewMeetings(){
+while true
+do
+echo -e "\e[0;36m${bold}Hint: Enter true or false to enable review_Meetings program ${normal}"
+echo -e "\e[0;38m${bold}please enter true or false. ${normal}"
+read review_Meetings
+        if ! [[ $review_Meetings == "true" || $review_Meetings == "false" ]]; then
+        echo -e "\e[0;31m${bold}Error - Please enter either true or false ${normal}"; fail=1
+                        else
+                        printf "reviewMeetings: $review_Meetings\n" >> program_selector.yml
+                        break;
+
+        fi
+done
+}
+
+check_pgi(){
+while true
+do
+echo -e "\e[0;36m${bold}Hint: Enter true or false to enable pgi program ${normal}"
+echo -e "\e[0;38m${bold}please enter true or false.${normal}"
+read pg_i
+        if ! [[ $pg_i == "true" || $pg_i == "false" ]]; then
+        echo -e "\e[0;31m${bold}Error - Please enter either true or false ${normal}"; fail=1
+                        else
+                        printf "pgi: $pg_i\n" >> program_selector.yml
+                        break;
+
+        fi
+done
+}
+
+check_pmPoshan(){
+while true
+do
+echo -e "\e[0;36m${bold}Hint: Enter true or false to enable pmPoshan program ${normal}"
+echo -e "\e[0;38m${bold}please enter true or false.${normal}"
+read pm_Poshan
+        if ! [[ $pm_Poshan == "true" || $pm_Poshan == "false" ]]; then
+        echo -e "\e[0;31m${bold}Error - Please enter either true or false ${normal}"; fail=1
+                        else
+                        printf "pgi: $pm_Poshan\n" >> program_selector.yml
+                        break;
+
+        fi
+done
+}
+
+check_udise(){
+while true
+do
+echo -e "\e[0;36m${bold}Hint: Enter true or false to enable udise program ${normal}"
+echo -e "\e[0;38m${bold}please enter true or false.${normal}"
+read ud_ise
+        if ! [[ $ud_ise == "true" || $ud_ise == "false" ]]; then
+        echo -e "\e[0;31m${bold}Error - Please enter either true or false ${normal}"; fail=1
+                        else
+                        printf "udise: $ud_ise\n" >> program_selector.yml
+                        break;
+
+        fi
+done
+}
+
+check_nas(){
+while true
+do
+echo -e "\e[0;36m${bold}Hint: Enter true or false to enable nas program ${normal}"
+echo -e "\e[0;38m${bold}please enter true or false.${normal}"
+read n_as
+        if ! [[ $n_as == "true" || $n_as == "false" ]]; then
+        echo -e "\e[0;31m${bold}Error - Please enter either true or false ${normal}"; fail=1
+                        else
+                        printf "nas: $n_as\n" >> program_selector.yml
+                        break;
+
+        fi
+done
+}
+
+check_diksha(){
+while true
+do
+echo -e "\e[0;36m${bold}Hint: Enter true or false to enable diksha program ${normal}"
+echo -e "\e[0;38m${bold}please enter true or false.${normal}"
+read dik_sha
+        if ! [[ $dik_sha == "true" || $dik_sha == "false" ]]; then
+        echo -e "\e[0;31m${bold}Error - Please enter either true or false ${normal}"; fail=1
+                        else
+                        printf "diksha: $dik_sha\n" >> program_selector.yml
+                        break;
+
+        fi
+done
+}
+
+check_nishtha(){
+while true
+do
+echo -e "\e[0;36m${bold}Hint: Enter true or false to enable nishtha program ${normal}"
+echo -e "\e[0;38m${bold}please enter true or false.${normal}"
+read nish_tha
+        if ! [[ $nish_tha == "true" || $nish_tha == "false" ]]; then
+        echo -e "\e[0;31m${bold}Error - Please enter either true or false ${normal}"; fail=1
+                        else
+                        printf "nishtha: $nish_tha\n" >> program_selector.yml
+                        break;
+
+        fi
+done
+}
+
+rm program_selector.yml
+touch program_selector.yml
+if [[ -e "program_selector.yml" ]]; then
+check_studentAttendance
+check_teacherAttendance
+check_reviewMeetings
+check_pgi
+check_pmPoshan
+check_udise
+check_nas
+check_diksha
+check_nishtha
+
+fi
+
+check_program_file(){
+if [[ -e "program_selector.yml" ]]; then
+        while true; do
+echo -e "\e[0;33m${bold}please preview the program_selector.yml file and confirm if everything is correct.${normal}"
+echo -e "\e[0;38m${bold} `cat program_selector.yml` ${normal}"
+echo -e "\e[0;33m${bold}Currently cQube program_selector.yml is entered. Follow Installation process with above config values.${normal}"
+echo -e "\e[0;33m${bold}If you want to edit config value please enter yes.${normal}"
+            while true; do
+
+             read -p "Do you still want to edit the config.yml file (yes/no)? " yn
+             case $yn in
+                 yes) break;;
+                 no) break 2;;
+                 * ) echo "Please answer yes or no.";;
+            esac
+            done
+             if [[ -e "program_selector.yml" ]]; then
+                          if [[ $yn == yes ]]; then
+                                 rm program.yml
+                                touch program.yml
+				check_studentAttendance
+				check_teacherAttendance
+				check_reviewMeetings
+				check_pgi
+				check_pmPoshan
+				check_udise
+				check_nas
+				check_diksha
+				check_nishtha
+                                
+fi
+             fi
+           done
+fi
+if [[ $yn == no ]]; then
+             echo -e "\e[0;32m${bold}program_selector file has been generated and validated successfully${normal}"
+
+    fi
+
+}
+
+
+check_program_file
+
