07fafe1d667d5301072183ee9c2951e445c926e7
asifbasha61 <asif.abdul@tibilsolutions.com>
2023-03-16 16:30:05 +0530

    minio and .env changes

--- shell_scripts/minio/install_minio.sh
+++ shell_scripts/minio/install_minio.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build -t cqube-minio-image .
+docker build -t cqube-minio-image ./shell_scripts/minio
 
-docker run -p 9000:9000 --name cqube_minio -d cqube-minio-image
+docker run -p 9000:9000 --name cqube_minio -d cqube-minio-image:latest
