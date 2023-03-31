dba5164a1cd5cd549abfae47cd4a653811e99af5
asifbasha61 <asif.abdul@tibilsolutions.com>
2023-03-31 09:19:24 +0530

    altered table after migration

--- shell_scripts/instantiate_static_processor_groups.sh
+++ shell_scripts/instantiate_static_processor_groups.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+db_user=$(awk ''/^db_user:' /{ if ($2 !~ /#.*/) {print $2}}' config.yml)
+db_name=$(awk ''/^db_name:' /{ if ($2 !~ /#.*/) {print $2}}' config.yml)
+
 echo "Instantiating the processor group in nifi"
 sleep 60
 sudo docker exec -i ansible_generator_app_1 bash << 'EOF'
@@ -8,4 +12,10 @@ EOF
 
 sudo docker exec -i ansible_nifi_app_1 bash << 'EOF'
 bash cqube-ingest-install.sh
+EOF
+
+sudo docker exec -i ansible_postgres_app_1 psql -U $db_user -d $db_name << 'EOF'
+ALTER TABLE spec."EventGrammar" ALTER  "instrumentField" DROP NOT NULL;
+ALTER TABLE spec."EventGrammar" ALTER  "instrumentType" DROP NOT NULL;
+ALTER TABLE spec."EventGrammar" ALTER  "dimensionMapping" DROP NOT NULL;
 EOF
