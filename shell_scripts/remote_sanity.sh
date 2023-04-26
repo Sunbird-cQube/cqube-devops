f9e3d6bee2db8c68d87416d5e2fe078454460e03
ayesha-siddiq123 <ayesha.siddiq@tibilsolutions.com>
2023-04-26 16:45:21 +0530

    upgradation for postgres data backup and restore the data

--- shell_scripts/remote_sanity.sh
+++ shell_scripts/remote_sanity.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 sudo docker cp postgres_app:/var/lib/postgresql/data /home/ubuntu/
-
+sudo chown -R ubuntu:ubuntu /home/ubuntu/data
 sudo docker stop $(sudo docker ps -aq)
 sudo docker rm $(sudo docker ps -aq)
 sudo docker rmi $(sudo docker images)
