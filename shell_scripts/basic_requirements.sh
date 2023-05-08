014f5208e6afaa3170249d413cc6102e63fe869d
asifbasha61 <asif.abdul@tibilsolutions.com>
2023-05-08 16:23:26 +0530

    changes

--- shell_scripts/basic_requirements.sh
+++ shell_scripts/basic_requirements.sh
@@ -2,11 +2,12 @@ sudo apt update -y && sudo apt upgrade -y
 sudo apt install software-properties-common -y
 sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt install ansible -y
-sudo apt install python3 -y
+sudo apt install python -y
 sudo apt install python3-pip -y
 sudo apt install unzip -y
 sudo apt install net-tools -y
 
 sudo apt install docker.io -y
+sudo pip install pyrsistent==0.16.1
 sudo pip3 install docker
 sudo pip install docker-compose
