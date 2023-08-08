af7ad16f5242026e08991ccba8275ac282bd0b2d
Tushar <codingid6@gmail.com>
2023-08-08 13:42:01 +0530

    download demo data zip

---
+++ vsk-data-seed-scripts/seed-data.sh
@@ -0,0 +1,7 @@
+bash ./seed_vsk_dimensions.sh
+bash ./seed_vsk_dimensions.sh
+
+wget https://drive.usercontent.google.com/download?id=1t1YYTzJeGyUrh1YZvk6d2uae39yDKj1c -O demo-data.zip
+unzip demo-data.zip
+rm demo-data.zip
+
